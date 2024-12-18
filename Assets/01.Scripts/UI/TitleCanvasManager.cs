01d8089b9ffca7544694f12c728fd8adb7789353
sinsanghyeon <sinsanghyeon1234@gmail.com>
2024-12-18 19:14:48 +0900

    feat StartScene
    added tmpro
    added font
    feat canvas
    added assets

---
+++ Assets/01.Scripts/UI/TitleCanvasManager.cs
@@ -0,0 +1,23 @@
+using SSH;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Application = UnityEngine.Device.Application;
+
+namespace BSM
+{
+    public class TitleCanvasManager : MonoBehaviour
+    {
+        [SerializeField]private OpenableCanvas SettingCanvas;
+        public string GameSceneName;
+        
+        public void StartGame()
+        {
+            SceneManager.LoadScene(GameSceneName);
+        }
+
+        public void EndGame()
+        {
+            Application.Quit();
+        }
+    }
+}
