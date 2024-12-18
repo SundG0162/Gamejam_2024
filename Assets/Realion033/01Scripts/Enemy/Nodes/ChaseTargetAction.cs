f5e38f74e5b030b360a54f7cf1a68da691dce369
Realion033 <purumisky@gmail.com>
2024-12-18 15:53:39 +0900

    feat : ChaseBT Fix

---
+++ Assets/Realion033/01Scripts/Enemy/Nodes/ChaseTargetAction.cs
@@ -0,0 +1,33 @@
+using BSM.Entities;
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using Unity.AppUI.Core;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "ChaseTarget", story: "[Mover] chase [Target] in [self]", category: "Action", id: "ec446077be2a671c8237b561aa495956")]
+public partial class ChaseTargetAction : Action
+{
+    [SerializeReference] public BlackboardVariable<EntityMover> Mover;
+    [SerializeReference] public BlackboardVariable<Transform> Target;
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+
+    protected override Status OnStart()
+    {
+        Vector3 direction = (Target.Value.position - Self.Value.transform.position).normalized;
+        Mover.Value.SetMovement(direction);
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
