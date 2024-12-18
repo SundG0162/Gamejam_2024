34a1c33cad6f678a113a1d7e943019119b0baeaa
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-19 01:21:10 +0900

    rename : SetParam -> SetParameter

--- Assets/01.Scripts/Animator/AnimateRenderer.cs
+++ Assets/01.Scripts/Animator/AnimateRenderer.cs
@@ -7,9 +7,9 @@ namespace BSM.Animators
         [SerializeField]
         protected Animator _animator;
 
-        public void SetParam(AnimatorParameterSO parameter, bool value) => _animator.SetBool(parameter.hashValue, value);
-        public void SetParam(AnimatorParameterSO parameter, float value) => _animator.SetFloat(parameter.hashValue, value);
-        public void SetParam(AnimatorParameterSO parameter, int value) => _animator.SetInteger(parameter.hashValue, value);
-        public void SetParam(AnimatorParameterSO parameter) => _animator.SetTrigger(parameter.hashValue);
+        public void SetParameter(AnimatorParameterSO parameter, bool value) => _animator.SetBool(parameter.hashValue, value);
+        public void SetParameter(AnimatorParameterSO parameter, float value) => _animator.SetFloat(parameter.hashValue, value);
+        public void SetParameter(AnimatorParameterSO parameter, int value) => _animator.SetInteger(parameter.hashValue, value);
+        public void SetParameter(AnimatorParameterSO parameter) => _animator.SetTrigger(parameter.hashValue);
     }
 }
