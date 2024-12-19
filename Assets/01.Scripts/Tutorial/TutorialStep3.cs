bbf383dfeeae2c97625553be535202068191d19f
sinsanghyeon <sinsanghyeon1234@gmail.com>
2024-12-19 23:58:59 +0900

    tutorial step3 making

--- Assets/01.Scripts/Tutorial/TutorialStep3.cs
+++ Assets/01.Scripts/Tutorial/TutorialStep3.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 namespace BSM.Tutorials
 {
+    
+    // 캐릭터 설명하는 튜토리얼
     public class TutorialStep3 : TutorialStep
     {
         public override void OnEnter()
         {
-            _tutorialManager.SetText("Shift로 대쉬하세요!");
+            _tutorialManager.SetBackground(false);
+            _tutorialManager.SetText("게임에는 총 3가지 캐릭터가 있습니다.");
         }
 
         public override void OnUpdate()
