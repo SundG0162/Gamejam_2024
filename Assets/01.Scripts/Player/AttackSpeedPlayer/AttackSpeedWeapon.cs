5dbc33ab3b4c11728ede13931d70e19f5d5ee657
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-19 09:51:55 +0900

    feat : AttackSpeedWeapon, AttackSpeedPlayer, Sprites, Stat Elements

---
+++ Assets/01.Scripts/Player/AttackSpeedPlayer/AttackSpeedWeapon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BSM.Players.AttackSpeedPlayer
+{
+    public class AttackSpeedWeapon : PlayerWeapon
+    {
+        private float _overheatAmount = 0f;
+        private float _overheatThreshold;
+    }
+}
