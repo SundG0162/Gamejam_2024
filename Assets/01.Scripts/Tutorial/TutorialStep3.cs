14d3fa3a445578b4eaa495dc1d46400c622ec9ae
sinsanghyeon <sinsanghyeon1234@gmail.com>
2024-12-20 00:48:19 +0900

    feat : tutorial step 4

--- Assets/01.Scripts/Tutorial/TutorialStep3.cs
+++ Assets/01.Scripts/Tutorial/TutorialStep3.cs
@@ -7,10 +7,43 @@ namespace BSM.Tutorials
     // 캐릭터 설명하는 튜토리얼
     public class TutorialStep3 : TutorialStep
     {
+        private int textIndex = 0;
+
+        public void SetIndexText()
+        {
+            Debug.Log(textIndex);
+            switch (textIndex)
+            {
+                case 0:
+                    _tutorialManager.SetText("<b>태그!<b>에는 총 3개의 캐릭터가 있습니다.");
+                    _tutorialManager.SetCharacterVisual(true);
+                    break;
+                case 1:
+                    _tutorialManager.SetText("무제한 방어력을 가진 캐릭터.\n 피해를 받지 않습니다. 5초후 자동 태그됩니다.");
+                    _tutorialManager.HighlightOneCharacter(0);
+                    break;
+                case 2:
+                    _tutorialManager.SetText("무제한 공격력을 가진 캐릭터.\n 모든 적을 한번에 죽입니다.");
+                    _tutorialManager.HighlightOneCharacter(1);
+                    break;
+                case 3:
+                    _tutorialManager.SetText("<무제한 공격속도를 가진 캐릭터.\n 빠른 공격으로 적을 밀쳐내 처치합니다.");
+                    _tutorialManager.HighlightOneCharacter(2);
+                    break;
+                case 4:
+                    _tutorialManager.SetCharacterVisual(false);
+                    _tutorialManager.NextTutorial();
+                    break;
+            }
+
+            textIndex++;
+        }
         public override void OnEnter()
         {
-            _tutorialManager.SetBackground(false);
-            _tutorialManager.SetText("게임에는 총 3가지 캐릭터가 있습니다.");
+            _tutorialManager.SetBackground(true);
+            _tutorialManager.SetCharacterVisual(true);
+            _tutorialManager.InputSO.OnMouseClickEvent += SetIndexText;
+            SetIndexText();
         }
 
         public override void OnUpdate()
@@ -20,6 +53,8 @@ namespace BSM.Tutorials
 
         public override void OnExit()
         {
+            _tutorialManager.SetBackground(false);
+            _tutorialManager.InputSO.OnMouseClickEvent -= SetIndexText;
         }
     }
 }
