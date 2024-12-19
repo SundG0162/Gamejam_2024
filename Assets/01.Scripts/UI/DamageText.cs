d2dbe205b0b9bb184670dbeec6ae79c7ff4a33c3
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-19 13:46:34 +0900

    feat : Serialized Dictionary Package

--- Assets/01.Scripts/UI/DamageText.cs
+++ Assets/01.Scripts/UI/DamageText.cs
@@ -18,7 +18,10 @@ namespace BSM.UI
                 _text.text = Mathf.CeilToInt(damage).ToString();
             _shadow.text = _text.text;
             _popupSequence = DOTween.Sequence();
-            transform.localScale = Vector3.one;
+            if (_text.text == "¡Ä")
+                transform.localScale = Vector2.one * 1.5f;
+            else
+                transform.localScale = new Vector2(0.3f, 0.3f) + Vector2.one * (damage / 4f);
             _popupSequence
                 .Append(transform.DOScale(0, 0.4f).SetEase(Ease.InCubic))
                 .Join(transform.DOMoveY(transform.position.y + 0.5f, 0.4f).SetEase(Ease.InCubic));
