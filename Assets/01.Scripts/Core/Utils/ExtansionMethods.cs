1f23410b2391c7f60c99a6a6d37177e5f7490d12
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-24 08:36:46 +0900

    feat : ExtansionMethod.RotateArray uses InPlaceAlgorithm

--- Assets/01.Scripts/Core/Utils/ExtansionMethods.cs
+++ Assets/01.Scripts/Core/Utils/ExtansionMethods.cs
@@ -5,6 +5,12 @@ namespace BSM.Utils
 {
     public static class ExtansionMethods
     {
+        /// <summary>
+        /// Rotate the array by count.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TArray">Target Array</param>
+        /// <param name="count">Count</param>
         public static void RotateArray<T>(this T[] TArray, int count)
         {
             if (TArray == null || TArray.Length == 0) return;
@@ -18,6 +24,12 @@ namespace BSM.Utils
                 PullArray(TArray, -count);
         }
 
+        /// <summary>
+        /// Push the array by count.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TArray">Target Array</param>
+        /// <param name="count">Count</param>
         public static void PushArray<T>(this T[] TArray, int count)
         {
             int length = TArray.Length;
@@ -31,6 +43,12 @@ namespace BSM.Utils
             Array.Copy(temp, 0, TArray, 0, count);
         }
 
+        /// <summary>
+        /// Puul the array by count.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TArray">Target Array</param>
+        /// <param name="count">Count</param>
         public static void PullArray<T>(this T[] TArray, int count)
         {
             int length = TArray.Length;
@@ -42,6 +60,59 @@ namespace BSM.Utils
             Array.Copy(TArray, 0, temp, 0, count);
             Array.Copy(TArray, count, TArray, 0, length - count);
             Array.Copy(temp, 0, TArray, length - count, count);
+        }
+
+        /// <summary>
+        /// Rotate the array by count. Use InPlace Algorithm.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TArray">Target Array</param>
+        /// <param name="count">Count</param>
+        public static void RotateArrayInPlace<T>(this T[] TArray, int count)
+        {
+            if(TArray == null || TArray.Length == 0) return;
+
+            count %= TArray.Length;
+            if (count == 0) return;
+
+            if (count > 0)
+                PushArrayInPlace(TArray, count);
+            else
+                PullArrayInPlace(TArray, -count);
+        }
+
+        public static void PushArrayInPlace<T>(this T[] TArray, int count)
+        {
+            int length = TArray.Length;
+            count %= length;
+            if (count == 0) return;
+
+            TArray.Reverse(0, length - 1);
+            TArray.Reverse(0, count - 1);
+            TArray.Reverse(count, length - 1);
+        }
+
+        public static void PullArrayInPlace<T>(this T[] TArray, int count)
+        {
+            int length = TArray.Length;
+            count %= length;
+            if (count == 0) return;
+
+            TArray.Reverse(0, count - 1);
+            TArray.Reverse(count, length - 1);
+            TArray.Reverse(0, length - 1);
+        }
+
+        private static void Reverse<T>(this T[] TArray, int start, int end)
+        {
+            while (start < end)
+            {
+                T temp = TArray[start];
+                TArray[start] = TArray[end];
+                TArray[end] = temp;
+                start++;
+                end--;
+            }
         }
     }
 }
