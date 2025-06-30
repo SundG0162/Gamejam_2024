93b796d0d3606cfec2f0a9156bbf753f3be47487
Realion033 <purumisky@gmail.com>
2025-07-01 08:49:16 +0900

    feat : InitPlayer RF

--- Assets/01.Scripts/Background/BackgroundMap.cs
+++ Assets/01.Scripts/Background/BackgroundMap.cs
@@ -10,7 +10,7 @@ namespace SSH
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            
         }
 
         // Update is called once per frame
